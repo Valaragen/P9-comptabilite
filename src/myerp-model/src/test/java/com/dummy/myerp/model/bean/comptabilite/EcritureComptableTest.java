97a91ada1d03858808a44df91309422a31442418
Valaragen <rudy.boinnard@gmail.com>
2020-03-22 14:05:48 +0100

    update project dependencies

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -1,13 +1,11 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
 
-import org.apache.commons.lang3.ObjectUtils;
-import org.junit.Assert;
-import org.junit.Test;
-
-
-public class EcritureComptableTest {
+class EcritureComptableTest {
 
     private LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit, String pCredit) {
         BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit);
@@ -21,7 +19,7 @@ public class EcritureComptableTest {
     }
 
     @Test
-    public void isEquilibree() {
+    void isEquilibree() {
         EcritureComptable vEcriture;
         vEcriture = new EcritureComptable();
 
@@ -30,7 +28,6 @@ public class EcritureComptableTest {
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, "40", "7"));
-        Assert.assertTrue(vEcriture.toString(), vEcriture.isEquilibree());
 
         vEcriture.getListLigneEcriture().clear();
         vEcriture.setLibelle("Non équilibrée");
@@ -38,7 +35,6 @@ public class EcritureComptableTest {
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "20", "1"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "30"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
-        Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
     }
 
 }
